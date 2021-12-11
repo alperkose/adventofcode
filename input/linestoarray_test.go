1f8d9a0212c34c7ac6db0e20140e56e5bbe33e37
Alper Köse <alperkose@gmail.com>
2021-12-11 22:12:49 +0100

    Input parsing mechanism

---
+++ input/linestoarray_test.go
@@ -0,0 +1,26 @@
+package input_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/alperkose/adventofcode2021/input"
+	"github.com/stretchr/testify/assert"
+)
+
+type TestContainer struct {
+	items []string
+}
+
+func (tc *TestContainer) Accept(in string) {
+	tc.items = append(tc.items, in)
+}
+
+func Test_ParseWithFunction(t *testing.T) {
+	sampleInput := "123\n456\n789"
+	testContainer := &TestContainer{}
+
+	input.Parse(strings.NewReader(sampleInput), testContainer)
+
+	assert.Equal(t, []string{"123", "456", "789"}, testContainer.items)
+}
